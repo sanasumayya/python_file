cbfe8203708adff003393eef17460ba095e39535
sanasumayya <sana.sumayya.ma@gmail.com>
2023-02-19 16:52:27 +0530

    first commit

---
+++ test_locators/locators.py
@@ -0,0 +1,24 @@
+class Sana_Locators:
+    username_inputBox = 'username'
+
+
+    password_InputBox = 'password'
+
+    password_InputBox2 = 'password'
+
+    LoginButton = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+    
+    button = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/div/div/button/i'
+
+    pimbutton='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span'
+
+
+    add = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[1]/button'
+
+    First_Name_inputbox = 'firstName'
+
+   # (//div[@class='oxd-table-card']//child::div[1]//child::div[9]//child::div[1]//button[2]/i)[1]
+
+   
+
+
